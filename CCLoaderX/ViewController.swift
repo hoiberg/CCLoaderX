4a919eff49c911f2c3052aa67462d423f40b3c8e
hoiberg <alex@marinuss-imac.fritz.box>
2017-10-08 16:17:09 +0200

    Initial Commit

---
+++ CCLoaderX/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CCLoaderX
+//
+//  Created by Alex on 08/10/2017.
+//  Copyright © 2017 Hangar42. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
